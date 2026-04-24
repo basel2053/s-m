e33163d43ab98197c2be0744f97eb1cfac7ee4ff
Bassel Salah <baselsalah2053@gmail.com>
2026-04-25 01:09:41 +0300

    chore: add .gitignore file

--- internal/api/handlers/auth_handler.go
+++ internal/api/handlers/auth_handler.go
@@ -5,7 +5,3 @@ import "net/http"
 func (h *Handler) Signup(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("User has been created"))
 }
-
-func (h *Handler) LogSom(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("User has been created"))
-}
